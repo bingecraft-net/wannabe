38ee6e4607fb4ad63ce115bddecc7e2c09bd5bfc
tyler <tyler@cruftbusters.com>
2022-12-30 15:58:24 -0800

    blow away mixin and add wannabe ingot

--- src/main/java/net/bingecraft/wannabe/Mod.java
+++ src/main/java/net/bingecraft/wannabe/Mod.java
@@ -1,21 +1,31 @@
 package net.bingecraft.wannabe;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Mod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("net-bingecraft-wannabe");
+  // This logger is used to write text to the console and the log file.
+  // It is considered best practice to use your mod id as the logger's name.
+  // That way, it's clear which mod wrote info, warnings, and errors.
+  public static final Logger LOGGER = LoggerFactory.getLogger("net-bingecraft-wannabe");
+  public static final Item wannabeIngot = Registry.register(
+    Registries.ITEM,
+    new Identifier("wannabe", "ingot"),
+    new Item(new FabricItemSettings())
+  );
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+  @Override
+  public void onInitialize() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    LOGGER.info("Hello Fabric world!");
+  }
 }
